b8f6f055666d75b4df6e93cd94cb943261b14d61
nguyentuan1696 <nguyentuan1696@gmail.com>
2025-01-30 23:56:14 +0700

    Refactor module imports and database connection configuration
    - Switch from ES modules to CommonJS
    - Add null coalescing for POSTGRES_URL
    - Configure SSL connection with rejectUnauthorized
    - Update import/require statements in main.js

--- lib.js
+++ lib.js
@@ -1,5 +1,9 @@
-import postgres from "postgres"
+const postgres = require('postgres');
 
-export const sql = postgres(process.env.POSTGRES_URL, {
-  ssl: "require",
-})
+const sql = postgres(process.env.POSTGRES_URL ?? "", {
+  ssl: {
+    rejectUnauthorized: false
+  },
+});
+
+module.exports = { sql };
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const schedule = require('node-schedule');
-const sql = require('./lib')
+const { sql } = require('./lib');
 const app = express();
 const port = 3000;
 
